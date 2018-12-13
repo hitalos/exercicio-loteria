3e5b1402e589a967c15bf116fa5b75c77079eee1
Hítalo Silva <hitalos@gmail.com>
2018-12-13 01:31:24 -0300

    Change input integers to strings

--- main.go
+++ main.go
@@ -3,20 +3,19 @@ package main
 import (
 	"bufio"
 	"os"
-	"strconv"
 )
 
-func combine(nums []int, list []int, start, end, index, size int) {
+func combine(nums []string, list []string, start, end, index, size int) {
 	if index == size {
 		for j := 0; j < size; j++ {
-			writer.WriteString(" " + strconv.Itoa(list[j]))
+			writer.WriteString(" " + list[j])
 		}
 		writer.WriteString("\n")
 		return
 	}
 
 	for i := start; i <= end && end >= size-index; i++ {
-		list[index] = nums[i]
+		list[index] = string(nums[i])
 		combine(nums, list, i+1, end, index+1, size)
 	}
 }
@@ -25,8 +24,8 @@ var writer *bufio.Writer
 
 func main() {
 	size := 8
-	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32}
-	list := make([]int, size)
+	nums := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32"}
+	list := make([]string, size)
 	writer = bufio.NewWriter(os.Stdout)
 	combine(nums, list, 0, len(nums)-1, 0, size)
 	writer.Flush()
