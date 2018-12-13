d33c76a22e7eea02982d98f7283e621dcc8e7870
Hítalo Silva <hitalos@gmail.com>
2018-12-13 01:27:01 -0300

    Change map to slice

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-func combine(nums []int, list map[int]int, start, end, index, size int) {
+func combine(nums []int, list []int, start, end, index, size int) {
 	if index == size {
 		for j := 0; j < size; j++ {
 			fmt.Fprint(writer, " ", list[j])
@@ -26,7 +26,7 @@ var writer *bufio.Writer
 func main() {
 	size := 8
 	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32}
-	list := make(map[int]int)
+	list := make([]int, size)
 	writer = bufio.NewWriter(os.Stdout)
 	combine(nums, list, 0, len(nums)-1, 0, size)
 	writer.Flush()
